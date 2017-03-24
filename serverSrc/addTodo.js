fd236bae9a807eb73922cab229ad7e9ee55978f3
Hyman_Chen <hyman_chen@wistron.com>
2017-03-24 13:54:22 +0800

    add initial server project template

---
+++ serverSrc/addTodo.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports.addTodo = (event, context, callback) => {
+  const todo = enent.body.todo;
+  const response = {
+    statusCode: 200
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
