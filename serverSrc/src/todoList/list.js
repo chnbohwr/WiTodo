9e153b75178657d98ea9a150388cd1cf6e7ba334
Amber_Chiu <Amber_Chiu@wistron.com>
2017-03-24 18:25:49 +0800

    feature/modified_create_update_delete_list_APIs

--- serverSrc/src/todoList/list.js
+++ serverSrc/src/todoList/list.js
@@ -1,5 +1,22 @@
 'use strict';
 
-module.exports.list = (event, context, callback) => {
+const todos = [
+  { 
+    todoId: 0,
+    todo: '制定API' 
+  },{
+    todoId: 1,
+    todo: 'serverless 初始架構'
+  }
 
+]
+
+exports.list = (event, context, callback) => {
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(todos)
+  };
+  
+  callback(null, response);    
 };
