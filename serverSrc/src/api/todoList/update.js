f4aab6bc8dc9d066fdeaa32d9d69312828a84f3f
Amber_Chiu <Amber_Chiu@wistron.com>
2017-03-28 11:54:21 +0800

    update APIs

--- serverSrc/src/api/todoList/update.js
+++ serverSrc/src/api/todoList/update.js
@@ -1,7 +1,32 @@
 'use strict';
+const promise = require('bluebird'); 
+
+const options = {
+    promiseLib: promise // overriding the default (ES6 Promise);
+};
+
+const pgp = require('pg-promise')(options);
+
 const Response = require('../../Response');
+const cn = require('../../../config');
 
 exports.update = (event, context, callback) => {
+  //const bodyData = JSON.parse(event.body);
 
-  callback(null, new Response(200));
+  const userId = 2;//bodyData.userId;
+  const todoId = 4;//bodyData.todoId;
+  const todo = '後端實做吧yayaya';//bodyData.todo;
+
+  const db = pgp('postgres://' + cn.user + ':'+ cn.password +'@' + cn.host + ':5432/' + cn.database);   
+
+  db.none('UPDATE todo_list SET todo = $1 WHERE todo_id = $2 AND user_id = $3', [todo, todoId, userId])
+    .then(data => {
+        callback(null, new Response(200)); 
+    })
+    .catch(error => {
+        callback(error); 
+    })
+    .finally(() => {
+        pgp.end(); 
+    });
 };
