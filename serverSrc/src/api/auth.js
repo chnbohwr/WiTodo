0f3ef9de55b13b2eecebb2e15832bb4cacb84f99
Andy Tsai <andy@Andy-Tsai.local>
2017-03-29 13:29:20 +0800

    add token detector

--- serverSrc/src/api/auth.js
+++ serverSrc/src/api/auth.js
@@ -19,9 +19,17 @@ exports.auth = (event, context, callback) => {
    * extra custom authorization logic here: OAUTH, JWT ... etc
    *
    */
+  let status = '';
+  if (token === '123') {
+    status = 'allow';
+  } else if (token === '456') {
+    status = 'deny';
+  } else if (token === '789') {
+    status = 'unauthorized';
+  }
 
   // In this example, the token is treated as the status for simplicity.
-  switch (token) {
+  switch (status) {
     case 'allow':
       callback(null, generatePolicy('user', 'Allow', event.methodArn));
       break;
