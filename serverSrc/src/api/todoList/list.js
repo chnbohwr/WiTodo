d914f802a1e5732730bd8e0daa82ce099558878c
Hyman_Chen <hyman_chen@wistron.com>
2017-03-30 10:09:54 +0800

    extract database to util folder

--- serverSrc/src/api/todoList/list.js
+++ serverSrc/src/api/todoList/list.js
@@ -1,29 +1,17 @@
 'use strict';
-const promise = require('bluebird'); 
-
-const options = {
-    promiseLib: promise // overriding the default (ES6 Promise);
-};
-
-const pgp = require('pg-promise')(options);
-
+const { pgp, db } = require('../../util/database');
 const Response = require('../../Response');
 
 exports.list = (event, context, callback) => {
-  //const bodyData = JSON.parse(event.body);
-  
+  //const bodyData = JSON.parse(event.body);  
   const userId = 1; //bodyData.userId;
-
-  const db = pgp(process.env.RDS_URL);
 
   db.any('SELECT * FROM todo_list WHERE user_id = $1', [userId])
     .then(data => {
-        callback(null, new Response(200, data));
-    })
-    .catch(error => {
-        callback(error); 
-    })
-    .finally(() => {
-        pgp.end(); 
-    });  
+      callback(null, new Response(200, data));
+    }).catch(error => {
+      callback(error);
+    }).finally(() => {
+      pgp.end();
+    });
 };
